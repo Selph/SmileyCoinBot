e7fe317a9200bfab3b28e43b549f092cb8252cda
ofurtumi <thorvaldur.tumi@gmail.com>
2022-12-12 22:14:17 +0000

    rock paper scissors

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
-const { Client, GatewayIntentBits, Partials } = require('discord.js')
-require ('dotenv/config')
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+require('dotenv/config');
+require('./commands/rps')
 
 const client = new Client({
 	intents: [
@@ -7,19 +8,44 @@ const client = new Client({
 		GatewayIntentBits.GuildMessages,
 		GatewayIntentBits.MessageContent,
 	],
-	partials: [
-		Partials.Message, Partials.Reaction
-	]
-})
+	partials: [Partials.Message, Partials.Reaction],
+});
+
+const possibilities = ['rock', 'paper', 'scissors'];
 
 client.on('ready', () => {
-	console.log('The bot is ready')
-})
+	console.log('The bot is ready');
+});
 
-client.on('messageCreate', message => {
-	if (message.content === 'ping') {
-		message.reply('pong')
+client.on('messageCreate', (message) => {
+	if (possibilities.includes(message.content)) {
+		let comp = Math.floor(Math.random() * 3);
+		let play = possibilities.indexOf(message.content);
+		let win = 0;
+
+		if ((play + 2) % 3 == comp) win = 1;
+		else if (comp == play) win = 2;
+
+		switch (win) {
+			case 0:
+				message.reply(
+					`you play ${possibilities[play]}, i play ${possibilities[comp]}.\n we are not the same.`
+				);
+				break;
+
+			case 1:
+				message.reply(
+					`you played ${message}, i played ${possibilities[comp]}. i never stood a chance`
+				);
+				break;
+
+			case 2:
+				message.reply(
+					`we both played ${message}, i guess great minds think alike :)`
+				);
+		}
+		message.reply(`comp: ${comp}`)
 	}
-})
+});
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
