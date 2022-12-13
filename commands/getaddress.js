e9738cbf6473e597c7193290e3edfbeb9ae2e0ff
Selph <72403805+Selph@users.noreply.github.com>
2022-12-13 04:39:46 +0000

    getaddress

--- commands/getaddress.js
+++ commands/getaddress.js
@@ -5,29 +5,14 @@ export const GetAddressCommand = new SlashCommandBuilder()
     .setDescription('Get the deposit address to deposit funds to your virtual wallet')
     .toJSON()
 
-export async function GetAddressInteraction(interaction, Wallets) {
+export async function SetAddressInteraction(interaction, Wallets) {
     let wallet = '';
     try {
         wallet = await Wallets.findOne({ where: { username: interaction.user.tag }})
-        await interaction.reply({
-            embeds: [{
-               title: `${interaction.user.username}'s Deposit Address`,
-               description: `wallet.address`,
-               color: "RANDOM"
-            }],
-            //this is the important part
-            ephemeral: true
-         });
+        await interaction.reply({ content: `wallet.address`, ephemeral: true})
     } catch(e){
         console.log(e);
-        await interaction.reply({
-            embeds: [{
-               title: `${interaction.user.username}'s Deposit Address`,
-               description: `Could not find a wallet for user ${interaction.user.tag}. Try \`/createwallet\` to create a new wallet.`,
-               color: "RANDOM"
-            }],
-            //this is the important part
-            ephemeral: true
-         });
+        interaction.reply({content:`Could not find a wallet for user ${interaction.user.tag}. Try \`/createwallet\` to create a new wallet.`, ephemeral: true });
+        
     }
 }     
