42aeffeb0faf752159a9429d5e638654177a1503
Selph <72403805+Selph@users.noreply.github.com>
2022-12-13 06:09:47 +0000

    withdraw

--- commands/withdraw.js
+++ commands/withdraw.js
@@ -1,4 +1,5 @@
 import { SlashCommandBuilder } from 'discord.js';
+import { isNull } from 'util';
 import { sendToAddress } from '../corewallet.js';
 
 export const WithdrawCommand = new SlashCommandBuilder()
@@ -11,24 +12,22 @@ export async function WithdrawInteraction(interaction, Wallets) {
     const name = interaction.user.tag;
     const amount = interaction.options.getInteger("amount");
     let wallet = '';
-    try {
-        wallet = await Wallets.findOne({ where: { username: name } });
-        if(wallet.balance >= amount){
-            const sendSmileys = sendToAddress(wallet.withdraw_address, amount);
-            if (wallet.withdraw_address === '') return interaction.reply({content: 'You have to set your withdraw address to withdraw funds. Use \`/setaddress\`'})
-            try{
-                const newBalance = wallet.balance - amount;
-                wallet = await Wallets.update({ balance: newBalance }, { where: { username: name } });
-            } catch (e) {
-                console.log(e);
-            }
-            interaction.reply({content: `Transferred ${amount}. Your balance is now ${wallet.balance}`, ephemeral: true});
+    
+    wallet = await Wallets.findOne({ where: { username: name } });
+    if (!wallet) return interaction.reply({content: `Could not find a wallet with name ${name}. Try \`/createwallet\` to create a new wallet.`, ephemeral: true});
+    if(wallet.balance >= amount){
+        const transaction = sendToAddress(wallet.withdraw_address, parseInt(amount));
+        if (wallet.withdraw_address === '') return interaction.reply({content: 'You have to set your withdraw address to withdraw funds. Use \`/setaddress\`'})
+        try{
+            const newBalance = wallet.balance - amount;
+            wallet = await Wallets.update({ balance: newBalance }, { where: { username: name } });
+        } catch (e) {
+            console.log(e);
         }
-        else{
-            interaction.reply({content: `Not enough smileys. Your balance is ${wallet.balance}.`, ephemeral: true})
-        }
-    } catch (e) {
-        console.log(e);
-        interaction.reply({content: `Could not find a wallet with name ${name}. Try \`/createwallet\` to create a new wallet.`, ephemeral: true});
+        interaction.reply({content: `Transferred ${amount}. Your balance is now ${wallet.balance}`, ephemeral: true});
     }
+    else{
+        interaction.reply({content: `Not enough smileys. Your balance is ${wallet.balance}.`, ephemeral: true})
+    }
+    
 }   
