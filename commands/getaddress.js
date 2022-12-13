e0b8310e7a7c969af1f0325b17f4a3c1c94e2d7f
Selph <72403805+Selph@users.noreply.github.com>
2022-12-13 04:40:59 +0000

    getaddress

--- commands/getaddress.js
+++ commands/getaddress.js
@@ -9,7 +9,7 @@ export async function GetAddressInteraction(interaction, Wallets) {
     let wallet = '';
     try {
         wallet = await Wallets.findOne({ where: { username: interaction.user.tag }})
-        await interaction.reply({ content: `wallet.address`, ephemeral: true})
+        await interaction.reply({ content: `${wallet.address}`, ephemeral: true})
     } catch(e){
         console.log(e);
         interaction.reply({content:`Could not find a wallet for user ${interaction.user.tag}. Try \`/createwallet\` to create a new wallet.`, ephemeral: true });
