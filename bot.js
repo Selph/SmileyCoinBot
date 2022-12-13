65b6ada5fc849e8211822a9c38bd082f03d4b149
Selph <72403805+Selph@users.noreply.github.com>
2022-12-13 01:24:33 +0000

    setaddress

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ import DeployCommands from './deploy-commands.js';
 import { Transactions, Wallets, SQLize } from './db.js'
 import { CreateWalletInteraction } from './commands/createwallet.js';
 import { CreateBalanceInteraction } from './commands/balance.js';
+import { SetAddressInteraction } from './commands/setaddress.js';
 
 const sequelize = SQLize
 const wallets = Wallets(sequelize)
@@ -22,7 +23,7 @@ client.on('interactionCreate', async (interaction) => {
     const { commandName } = interaction
     if (commandName === 'ping') interaction.reply({ content: 'Pong!'})
     if (commandName === 'createwallet') await CreateWalletInteraction(interaction, wallets)
-    if (commandName === 'setaddress') interaction.reply({ content: 'Todo!'})
+    if (commandName === 'setaddress') await SetAddressInteraction(interaction, wallets)
     if (commandName === 'balance') await CreateBalanceInteraction(interaction, wallets)
     if (commandName === 'tip') interaction.reply({ content: 'Todo!'})
     if (commandName === 'withdraw') interaction.reply({ content: 'Todo!'})
